b452ccc12c6cceb2406c0ec38324c0187fbbcdfc
jarefu1 <jarefjesus@gmail.com>
2026-09-17 04:15:02 -0500

    feat(catalogo): personalizar titulo con marca vista

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -13,7 +13,7 @@ function Catalogo() {
   return (
     <main className="catalogo">
       <div className="catalogo__header">
-<h1 className="catalogo__title">Catálogo Retro. ✦</h1>        <input
+<h1 className="catalogo__title">vista lista</h1>        <input
           type="text"
           placeholder="Buscar mochila..."
           className="catalogo__search"
